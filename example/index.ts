58ba528a19e1f7f85fb584dd86468f669434e0a9
Huynh Duc Dung <dunghd.it@gmail.com>
2023-08-01 23:01:41 +0800

    chore(deps): add consola logger

--- example/index.ts
+++ example/index.ts
@@ -1,5 +1,7 @@
+import { consola } from "consola";
+
 import { downloadVideo } from "../src";
 
 downloadVideo("https://twitter.com/mattpocockuk/status/1592130978234900484")
-  .then(console.log)
-  .catch(console.error);
+  .then(consola.log)
+  .catch(consola.error);
